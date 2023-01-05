403d4f9c10b5a071929970abf5baad7ef104a1b0
Mauricio Aparecido Picirillo <79844553+mauriciopicirillo@users.noreply.github.com>
2023-01-05 11:07:16 -0300

    Adicionado retorno de lista por Json. retorno de Http404 e configurado timezone para pt-br

--- agenda/settings.py
+++ agenda/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1bacy(*#m1t4lxg0&c@%%2w27@t!lc=0oll@@5j=+$0gw7167h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,13 +104,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
